1bad3ff605f4abe6b894a356cdb326f4d5f7d986
Paris Woods <148830068+pwoods125@users.noreply.github.com>
2024-02-02 00:01:31 -0500

    WIP-JavaScriptNeedHelp

---
+++ assets/js/script.js
@@ -0,0 +1,87 @@
+// GIVEN a weather dashboard with form inputs
+//  and 
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+
+var userSearchFormEl = document.getElementById('user-search-form');
+var prevCityButtonsEl = document.getElementById('previous-cities-buttons');
+
+var searchBtnEl = document.getElementById("searchbtn");
+
+var citynameEl = document.getElementById("cityname");
+var city = document.querySelector(".city");
+var dateEl = document.querySelector(".date");
+var temp = document.querySelector(".temp");
+var forecast = document.querySelector(".forecast");
+var icon = document.querySelector(".icon");
+var humidity = document.querySelector(".humidity");
+var windspeed = document.querySelector(".windspeed");
+
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city
+
+function userInput(event){
+  event.preventDefault();
+// capture user input
+  var citynameValue = citynameEl.value
+  getWeather(citynameValue)
+}
+
+function getWeather(x){
+       fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + x + "&appid=f188485bc265ae795fef301027c27bbc&units=imperial")
+        .then(function (response) {
+            return response.json();
+        }).then(function(data){
+            city.textContent = data.city.name
+            var days = data.list
+            $.each($('.weather-container'), function(i, container) { 
+              container.child.val(days[i].dt_txt)
+              dateEl.textContent = days[i].dt_txt
+              temp.textContent = days[i].main.temp
+              forecast.textContent = days[i].weather[0].main
+              icon.src = "http://openweathermap.org/img/wn/" + days[i].weather[0].icon + ".png"
+              humidity.textContent = days[i].main.humidity + "%"
+              windspeed.textContent = days[i].wind.speed
+          });
+          //   $('.weather-container').each(function() {
+          //     var i = 0
+          //     dateEl.textContent = days[i].dt_txt
+          //     temp.textContent = days[i].main.temp
+          //     forecast.textContent = days[i].weather[0].main
+          //     icon.src = "http://openweathermap.org/img/wn/" + days[i].weather[0].icon + ".png"
+          //     humidity.textContent = days[i].main.humidity + "%"
+          //     windspeed.textContent = days[i].wind.speed
+          //     i++
+          //  });
+            // for (var i = 0; i < 5; i++){
+            //   dateEl.textContent = days[i].dt_txt
+            //   temp.textContent = days[i].main.temp
+            //   forecast.textContent = days[i].weather[0].main
+            //   icon.src = "http://openweathermap.org/img/wn/" + days[i].weather[0].icon + ".png"
+            //   humidity.textContent = days[i].main.humidity + "%"
+            //   windspeed.textContent = days[i].wind.speed
+            // };
+            console.log(data);
+        }); 
+}
+
+
+searchBtnEl.addEventListener("click",userInput);
+
+
+
+// WHEN I search for a city that city is added to the search history
+  // WHEN I click the save button for that timeblock
+  // THEN the text for that event is saved in local storage
+  // WHEN I refresh the page
+  // THEN the saved events persist
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+
+
+  
+
